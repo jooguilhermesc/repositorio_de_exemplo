620bf3b066bed056503edaad734259be81e032de
João Guilherme Silva Cabral <46795337+jooguilhermesc@users.noreply.github.com>
2022-09-08 22:07:48 -0300

    Atualiza programa para perguntar como o usuário está
    Dependendo da personalização do nome, programa pergunta se o o usuário está se sentindo bem

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 name = input("Qual seu nome? ")
 
 if name == "João":
-    print(f"Olá {name}, tudo bem com você?")
+    status = input(f"Olá {name}, tudo bem com você? ")
+    if status == "Sim":
+        print("Ainda bem!")
+    else:
+        print("Que pena, espero que melhore...")
 else:
     print(f"Olá {name}, é a sua primeira vez acessando esta máquina?")
